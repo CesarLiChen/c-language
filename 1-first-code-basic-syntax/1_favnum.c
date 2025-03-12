b11efdc776938e841c9fa7cafb7c531c944c8032
Cesar <cesarli619@gmail.com>
2025-03-12 15:25:26 -0400

    First commit

---
+++ 1-first-code-basic-syntax/1_favnum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	int myFavNum = 0;
+	
+	printf("Please enter your favourite number\n");
+	scanf("%d", &myFavNum);
+	printf("You entered: %d\n", myFavNum);
+
+	return 0;
+}
---
+++ 1-first-code-basic-syntax/challenge.c
@@ -0,0 +1,8 @@
+// Write a C program that displays your first name as output
+
+#include <stdio.h>
+
+int main() {
+    printf("Hi, my name is Cesar\n");
+    return 0;
+}
